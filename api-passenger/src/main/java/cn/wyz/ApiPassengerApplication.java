af745ece574e0990e0acc224c1bb3600d61e2761
wangnanxiang <806543499@qq.com>
2022-10-23 13:58:11 +0800

    基本环境搭建

---
+++ api-passenger/src/main/java/cn/wyz/ApiPassengerApplication.java
@@ -0,0 +1,14 @@
+package cn.wyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangnanxiang
+ */
+@SpringBootApplication
+public class ApiPassengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+}
